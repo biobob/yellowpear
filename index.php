ac291472204f74c2f43097022ac12b56edd1497a
Robert Komorovsky <robert.komorovsky@gmail.com>
2014-01-23 15:54:44 +0100

    Removed unnecessary whitespaces

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="<?php echo  substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2); ?>">
+<html lang="<?php echo substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);?>">
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
